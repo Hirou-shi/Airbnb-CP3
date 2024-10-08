d3ab3a1a37788cbc62c0905291cce4880ba63b09
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-08 13:59:36 -0300

    3

--- airbnbCopy/src/main.jsx
+++ airbnbCopy/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
+import Home from './pages/home.jsx';
+
 
 
 const Rotas = createBrowserRouter ([
