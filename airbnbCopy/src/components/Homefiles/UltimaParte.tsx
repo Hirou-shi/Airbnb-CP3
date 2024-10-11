da49f90a4110bae7261f2f7c8857d43498e9aefc
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-11 16:26:08 -0300

    Parte final

---
+++ airbnbCopy/src/components/Homefiles/UltimaParte.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function UltimaParte() {
+  return (
+    <section>
+        <div>
+            <div className='flex flex-col text-left'>
+                <h1 className='font-semibold'>Destaque do bairro</h1>
+                <p>Desde 1986, o Museu d'Orsay é um dos principais destinos parisienses para os amantes da arte. Ele abriga a maior coleção de arte impressionista e pós-impressionista do mundo, com pintores renomados como Manet, Monet, Degas, Renoir, Seurat, Gauguin e Van Gogh. Ele também conta com uma vista inigualável da Cerimônia de Abertura dos Jogos Olímpicos de Paris 2024. Sob minha orientação, o terraço foi transformado em um encantador jardim secreto, oferecendo o cenário perfeito para participar deste grande espetáculo histórico na Cidade Luz.</p>
+            </div>
+        </div>
+    </section>
+  )
+}
