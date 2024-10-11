a33f118724300d5bb5a27237759d05b493fc696f
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-11 16:54:01 -0300

    Ultima parte finalizada

--- airbnbCopy/src/components/Homefiles/UltimaParte.tsx
+++ airbnbCopy/src/components/Homefiles/UltimaParte.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Mail, CircleCheck, IdCard } from 'lucide-react'
 
 export default function UltimaParte() {
   return (
@@ -13,6 +14,22 @@ export default function UltimaParte() {
                 <img className='w-70 h-50' src="Iconico.png" alt="medalha" />
                 <h1 className='font-bold text-[60px]'>Icônicos</h1>
                 <p className='w-[390px] text-[15px]'>Experiências extraordinárias oferecidas pelos maiores nomes da música, do cinema, da TV, da arte, dos esportes e muito mais.</p>
+            </div>
+            <div className='grid grid-cols-3 p-5'>
+                <div className=''>
+                    <Mail />
+                    <h1 className='font-semibold text-sm'>Pedido de reserva</h1>
+                    <p className='text-[12px] text-gray-500'>Escolha suas datas, inclua seus hóspedes e responda por que você deseja ir.</p>
+                </div>
+                <div>
+                    <CircleCheck/><h1 className='font-semibold text-sm'>Processo de seleção</h1>
+                    <p className='text-[12px] text-gray-500'>Primeiro, escolheremos aleatoriamente um conjunto de hóspedes em potencial. Em seguida, analisaremos suas respostas para entender suas perspectivas e conexão com o ícone. Por fim, convidaremos os hóspedes selecionados a reservarem.</p></div>
+                <div>
+                    <IdCard /><h1 className='font-semibold text-sm'>Requisitos</h1>
+                    <p className='text-[12px] text-gray-500'>Para participar, você precisa ter uma conta ativa no Airbnb, o aplicativo do Airbnb e ser residente em um país ou região elegível. A participação é gratuita.</p></div>
+            </div>
+            <div className='border-y-[1px] border-t-gray-300 p-5'>
+                <p className='text-[8px] text-gray-500 text-left w-[60%]'>Se seu perfil for selecionado e você decidir reservar, você terá 24 horas para concluir a reserva. Os custos de viagem não estão inclusos. Confira as regras completas, incluindo os requisitos de idade e localização geográfica, como os seus dados serão utilizados, e demais condições aplicáveis.</p>
             </div>
         </div>
     </section>
