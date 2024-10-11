a04798d9c5e0defe81dae4b6fbb61a6a9b70d0b8
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-10-11 16:20:59 -0300

    Acabado onde você estará

--- airbnbCopy/src/components/Homefiles/Ondevcestara.tsx
+++ airbnbCopy/src/components/Homefiles/Ondevcestara.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 export default function Ondevcestara() {
   return (
     <section>
-        <h1 className='font-semibold'>Onde você estará</h1>
-        <p>Paris, Ilha de França, França</p>
-        <div>
-            <img src="/localizacao-cp.png" alt="localização"/>
-        </div>
-        <div className='flex'>
-            <p>Verificamos que a localização deste anúncio está correta.</p>
-            <p className='underline font-semibold'>Saiba mais</p>
+        <div className='text-left'>
+            <h1 className='font-semibold text-[20px] mb-4'>Onde você estará</h1>
+            <p className=''>Paris, Ilha de França, França</p>
+            <div className='py-3'>
+                <img src="/localizacao-cp.png" alt="localização"/>
+            </div>
+            <div className='flex py-4 gap-1'>
+                <p>Verificamos que a localização deste anúncio está correta.</p>
+                <p className='underline font-semibold'>Saiba mais</p>
+            </div>
         </div>
     </section>
   )
